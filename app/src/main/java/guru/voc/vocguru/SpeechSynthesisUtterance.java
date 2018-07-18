67c687ceb056f4a3b252c3c6b42c5fb9fba3dcc3
alexeenka <4alexey.alexeenka@gmail.com>
2018-07-18 15:44:47 +0300

    initial version

---
+++ app/src/main/java/guru/voc/vocguru/SpeechSynthesisUtterance.java
@@ -0,0 +1,24 @@
+package guru.voc.vocguru;
+
+public class SpeechSynthesisUtterance {
+
+    private String text;
+
+    private String lang;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+}
